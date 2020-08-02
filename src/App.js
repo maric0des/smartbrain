1fb06469aeaaae9b6f4cd79caa941a8c679d27b3
maric0des <maricodes.dev@gmail.com>
2020-08-02 14:41:17 +0900

    initial commit

---
+++ src/App.js
@@ -0,0 +1,197 @@
+import React, { Component } from 'react';
+import Particles from 'react-particles-js';
+import Navigation from './components/navigation/Navigation'
+import FaceRecognition from './components/FaceRecognition/FaceRecognition'
+import Signin from './components/Signin/Signin'
+import Register from './components/Register/Register'
+import Logo from './components/logo/Logo';
+import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
+import Rank from './components/Rank/Rank';
+import './App.css';
+
+const particlesOptions = {
+  "particles": {
+    "number": {
+        "value": 160,
+        "density": {
+            "enable": false
+        }
+    },
+    "size": {
+        "value": 3,
+        "random": true,
+        "anim": {
+            "speed": 4,
+            "size_min": 0.3
+        }
+    },
+    "line_linked": {
+        "enable": false
+    },
+    "move": {
+        "random": true,
+        "speed": 1,
+        "direction": "top",
+        "out_mode": "out"
+    }
+  },
+  "interactivity": {
+      "events": {
+          "onhover": {
+              "enable": true,
+              "mode": "bubble"
+          },
+          "onclick": {
+              "enable": true,
+              "mode": "repulse"
+          }
+      },
+      "modes": {
+          "bubble": {
+              "distance": 250,
+              "duration": 2,
+              "size": 0,
+              "opacity": 0
+          },
+          "repulse": {
+              "distance": 400,
+              "duration": 4
+          }
+      }
+  }
+}
+const initialState = {
+  input: '',
+  imageUrl: '',
+  box: {},
+  route: 'signin',
+  isSignedIn: false,
+  user: {
+    id: '',
+    name:  '',
+    email: '',
+    entries: 0,
+    joined: ''
+  }
+}
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageUrl: '',
+      box: {},
+      route: 'signin',
+      isSignedIn: false,
+      user: {
+        id: '',
+        name:  '',
+        email: '',
+        entries: 0,
+        joined: ''
+      }
+    }
+  }
+
+  loadUser = (data) => {
+    this.setState({
+      user: {
+        id: data.id,
+        name: data.name,
+        email: data.email,
+        entries: data.entries,
+        joined: data.joined,
+      }
+    })
+  }
+
+  onInputChange = (event) => {
+    this.setState({ input: event.target.value })
+  }
+
+  onButtonSubmit = () => {
+    this.setState({ imageUrl: this.state.input });
+    fetch('https://cryptic-ocean-54672.herokuapp.com/imageurl', {
+      method: 'post',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({
+          input: this.state.input
+      })
+    })
+    .then(response => response.json())
+    .then(response =>  {
+      if (response) {
+        fetch('https://cryptic-ocean-54672.herokuapp.com/image', {
+          method: 'put',
+          headers: {'Content-Type': 'application/json'},
+          body: JSON.stringify({
+              id: this.state.user.id
+          })
+        })
+          .then(response => response.json())
+          .then(count => {
+            this.setState(Object.assign(this.state.user, { entries: count }))
+          })
+          .catch(console.log)
+      }
+      this.displayFaceBox(this.calculateFaceLocation(response));
+    })
+    .catch(err => console.log(err))
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputimage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - (clarifaiFace.right_col * width),
+      bottomRow: height - (clarifaiFace.bottom_row * height)
+    };
+  }
+
+  onRouteChange = (route) => {
+    if (route === 'signin') {
+      this.setState(initialState);
+    } else if (route === 'home') {
+      this.setState({isSignedIn: true})
+    }
+    this.setState({ route: route })
+  }
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  }
+
+  render() {
+    const { isSignedIn, imageUrl, route, box, user } = this.state;
+    return (
+      <div className="App">
+        <Particles
+          className="particles"
+          params={particlesOptions}
+        />
+        <Navigation onRouteChange={this.onRouteChange} isSignedIn={isSignedIn} />
+        { route === 'home'
+        ? <>
+        <Logo />
+        <Rank name={user.name} entries={user.entries} />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition box={box} imageUrl={imageUrl} />
+        </>
+        : ( route === 'signin'
+            ? <Signin onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+            : <Register onRouteChange={this.onRouteChange} loadUser={this.loadUser} />
+          )
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
